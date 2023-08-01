3800a57fe85c8f88820c5e43ac80f0c7df83afdf
hidouche ait youcef <hidouche.aityoucef@gmail.com>
2023-08-01 07:44:29 +0200

    first commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,18 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import HomePage from '../views/HomePage.vue'
+import BasketPage from '../views/BasketPage.vue'
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
+    name: 'HomePage',
+    component: HomePage
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+    path: '/basketPage',
+    name: 'BasketPage',
+    component: BasketPage
+  },
 ]
 
 const router = createRouter({
--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,57 @@
 import { createStore } from 'vuex'
+import axios from 'axios'
 
 export default createStore({
   state: {
+    products: [],
+    productsInBag: []
   },
+
   getters: {
   },
+
   mutations: {
+    loadProducts(state, products) {
+      state.products = products;
+    },
+    loadBag(state, products) {
+      state.productsInBag = products;
+    },
+    addToBag(state, product) {
+      state.productsInBag.push(product);
+      localStorage.setItem('productsInBag',JSON.stringify(state.productsInBag));
+    },
+    removeFromBag(state, productId) {
+      let updatedBag = state.productsInBag.filter(item => productId != item.id);
+      state.productsInBag = updatedBag;
+      localStorage.setItem('productsInBag',JSON.stringify(state.productsInBag));
+    }
   },
+
   actions: {
+    loadProducts({ commit }) {
+      axios
+      .get('https://fakestoreapi.com/products?limit=18')
+      .then(response => {
+        commit('loadProducts', response.data);
+      })
+    },
+    loadBag({ commit }) {
+      if (localStorage.getItem('productsInBag')) {
+        commit('loadBag', JSON.parse(localStorage.getItem('productsInBag')));
+      }
+      
+    },
+    addToBag({ commit }, product) {
+      commit('addToBag', product);
+    }, 
+    removeFromBag({ commit }, productId) {
+      if (confirm('Are you sure you want remove the item from bag ?')) {
+        commit('removeFromBag', productId);
+      }
+    }
   },
+
   modules: {
   }
 })
